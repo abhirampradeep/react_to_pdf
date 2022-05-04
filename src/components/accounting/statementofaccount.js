7553b45bf952064f0acbaee44ac4ba78de2cb944
abhirampradeep <abhiramkpradeep123@gmail.com>
2022-05-04 18:46:38 +0530

    statemtofaccount and headofaccount

---
+++ src/components/accounting/statementofaccount.js
@@ -0,0 +1,221 @@
+import React from 'react';
+
+class Statementaccount extends React.Component {
+    render() {
+        return (
+            <div>
+
+                {/* header starts */}
+                <h1 className="text-left p-3 ml-2 text-lg">
+                    Mananthavady Govt Employees Co-op Society Ltd W 288
+                </h1>
+
+                <div className="flex justify-end text-right mr-2">
+                    <h3>
+                        Report ON
+                    </h3>
+                    <h3 className="ml-2">
+                        19/04/2022
+                    </h3>
+                </div>
+
+                <div className="flex justify-between" >
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Day Book On
+                        </h3>
+                        <h3 className="ml-2">
+                            12/04/2022
+                        </h3>
+                    </div>
+                    <div>
+                        <h3 className="p-3 mr-2">page #1</h3>
+                    </div>
+                </div>
+                {/* header ends */}
+
+
+
+                {/* user account starts */}
+                <div className="flex justify-start" >
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Account NO:
+                        </h3>
+                        <h3 className="ml-2">
+                            85
+                        </h3>
+                    </div>
+
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Name:
+                        </h3>
+                        <h3 className="ml-2">
+                            Abhiram
+                        </h3>
+                    </div>
+                </div>
+
+
+                <div className="flex justify-end" >
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Opening Balance
+                        </h3>
+                        <h3 className="ml-2 ">
+                            117.00
+                        </h3>
+                    </div>
+                </div>
+
+                <table className="border-4 w-full p-1 table-auto ">
+
+                    <tr className="border-4">
+                        <td className="border-4">Date</td>
+                        <td className="border-4">Voucher NO</td>
+                        <td className="border-4">A/C</td>
+                        <td className="border-4">Narration</td>
+                        <td className="text-right">Cheque</td>
+                        <td className="text-left">Details</td>
+                        <td className="border-4">Deposits</td>
+                        <td className="border-4">Withdrawal</td>
+                        <td className="border-4">Balance</td>
+                    </tr>
+
+                    <tr className="border-4">
+                        <td className="border-4">18/04/2022</td>
+                        <td className="border-4">234</td>
+                        <td className="border-4">C</td>
+                        <td className="border-4">By cash</td>
+                        <td className="text-right border-4"></td>
+                        <td className="text-left border-4"></td>
+                        <td className="border-4">22,170.00</td>
+                        <td className="border-4"></td>
+                        <td className="border-4">22,287.00</td>
+                    </tr>
+
+                    <tr className="border-4">
+                        <td className="border-4">19/04/2022</td>
+                        <td className="border-4">234</td>
+                        <td className="border-4">C</td>
+                        <td className="border-4">To Loose Leaf</td>
+                        <td className="text-right border-4"></td>
+                        <td className="text-left border-4"></td>
+                        <td className="border-4"></td>
+                        <td className="border-4">5000</td>
+                        <td className="border-4">17,287.00</td>
+                    </tr>
+
+                </table>
+
+                <div className="flex justify-end" >
+                    <div className="flex text-left p-3 ml-2 underline">
+                        <h3>
+                            Closing Balance
+                        </h3>
+                        <h3 className="ml-2 underline">
+                            17,287.00
+                        </h3>
+                    </div>
+                </div>
+
+                {/* user accounts ends */}
+                <div className="flex justify-start" >
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Account NO:
+                        </h3>
+                        <h3 className="ml-2">
+                            85
+                        </h3>
+                    </div>
+
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Name:
+                        </h3>
+                        <h3 className="ml-2">
+                            Abhiram
+                        </h3>
+                    </div>
+                </div>
+
+
+                <div className="flex justify-end" >
+                    <div className="flex text-left p-3 ml-2">
+                        <h3>
+                            Opening Balance
+                        </h3>
+                        <h3 className="ml-2 ">
+                            117.00
+                        </h3>
+                    </div>
+                </div>
+
+                <table className="border-4 w-full p-1 table-auto ">
+
+                    <tr className="border-4">
+                        <td className="border-4">Date</td>
+                        <td className="border-4">Voucher NO</td>
+                        <td className="border-4">A/C</td>
+                        <td className="border-4">Narration</td>
+                        <td className="text-right">Cheque</td>
+                        <td className="text-left">Details</td>
+                        <td className="border-4">Deposits</td>
+                        <td className="border-4">Withdrawal</td>
+                        <td className="border-4">Balance</td>
+                    </tr>
+
+                    <tr className="border-4">
+                        <td className="border-4">18/04/2022</td>
+                        <td className="border-4">234</td>
+                        <td className="border-4">C</td>
+                        <td className="border-4">By cash</td>
+                        <td className="text-right border-4"></td>
+                        <td className="text-left border-4"></td>
+                        <td className="border-4">22,170.00</td>
+                        <td className="border-4"></td>
+                        <td className="border-4">22,287.00</td>
+                    </tr>
+
+                    <tr className="border-4">
+                        <td className="border-4">19/04/2022</td>
+                        <td className="border-4">234</td>
+                        <td className="border-4">C</td>
+                        <td className="border-4">To Loose Leaf</td>
+                        <td className="text-right border-4"></td>
+                        <td className="text-left border-4"></td>
+                        <td className="border-4"></td>
+                        <td className="border-4">5000</td>
+                        <td className="border-4">17,287.00</td>
+                    </tr>
+
+                </table>
+
+                <div className="flex justify-end" >
+                    <div className="flex text-left p-3 ml-2 underline">
+                        <h3>
+                            Closing Balance
+                        </h3>
+                        <h3 className="ml-2 underline">
+                            17,287.00
+                        </h3>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+            </div>
+        )
+    }
+}
+
+export default Statementaccount;
--- src/export-pdf.component.js
+++ src/export-pdf.component.js
@@ -3,6 +3,8 @@ import ReactToPrint from 'react-to-print';
 import Accounthead from './components/accounting/accounthead';
 import Daybook from './components/accounting/daybook';
 import Generallegder from './components/accounting/generalledger';
+import Headofaccount from './components/accounting/headofaccount';
+import Statementaccount from './components/accounting/statementofaccount';
 import Dividendpayable from './components/dividend/dividendpayable';
 import ShareHistory from './components/membership/sharehistory';
 import TableComponent from './table.component';
@@ -15,7 +17,7 @@ class ExportPdfComponent extends React.Component {
 
            {/* <h1 className=" text-center">Transaction History</h1>  */}
 
-          <ShareHistory ref={(response) => (this.componentRef = response)} />
+          <Statementaccount ref={(response) => (this.componentRef = response)} />
           
           <ReactToPrint
             content={() => this.componentRef}
